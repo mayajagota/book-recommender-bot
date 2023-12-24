3346cbf80637a93c92578241c44edb2560af43b9
Maya Jagota <mayajagota@ucsb.edu>
2023-12-24 00:34:18 -0800

    initial commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+BOT_TOKEN = 'MTE4ODM5MDk3OTgyOTUxMDE2NA.Gkeben.Te_BxWaA5IZwkPJ8PivPSn_gP4Uz5o-QAiQKqY'
+CHANNEL_ID = 1188393262298439720
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("hi! book recommender bot is ready! :)")
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send("hi! :)")
+
+@bot.command()
+async def recommend(ctx, genre):
+    result = genre
+    await ctx.send(f"{result}")
+bot.run(BOT_TOKEN)
